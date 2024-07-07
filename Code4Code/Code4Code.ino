60fec3fad71348ce976971503843dc36aa7cca46
Defragster <Defragster@users.noreply.github.com>
2024-07-07 13:13:02 -0700

    DevBoard 528+1 and @4K

--- Code4Code/Code4Code.ino
+++ Code4Code/Code4Code.ino
@@ -5,12 +5,12 @@
 IntervalTimer Alpha;
 char szTeensy[24] = "";
 
-#define PI_DIGITS 15 // sets of 4
-#define PI_DIGITS_SZ  (PI_DIGITS*4) + 4 // compare string
-#define CODE_PLACE FLASHMEM // as NULL or FLASHMEM 
-#define DATA_PLACE PROGMEM // as FASTRUN or PROGMEM 
-#define PI_INLINE  inline  // Set to 'inline' for seePi() inline
-const char szPiDigits[] = "314159265358979323846264338327950288419716939937510582097494"; // 60 digits ONLY for PI_DIGITS 15
+#define PI_DIGITS 15                                                                       // sets of 4
+#define PI_DIGITS_SZ (PI_DIGITS * 4) + 4                                                   // compare string
+#define CODE_PLACE FLASHMEM                                                                // as NULL or FLASHMEM
+#define DATA_PLACE PROGMEM                                                                 // as FASTRUN or PROGMEM
+#define PI_INLINE inline                                                                   // Set to 'inline' for seePi() inline
+const char szPiDigits[] = "314159265358979323846264338327950288419716939937510582097494";  // 60 digits ONLY for PI_DIGITS 15
 const uint32_t sumPi60dig = 75967;
 // DEBUG TEST
 //const char szPiDigits[] = "31415926535897932384626433832795028841971693993751058209749"; // 60 digits ONLY for PI_DIGITS 15
@@ -19,148 +19,155 @@ const uint32_t sumPi60dig = 75967;
 uint32_t time_now;
 uint32_t piCycles;
 uint32_t isrCycles, allCycles;
+extern "C" uint32_t set_arm_clock(uint32_t frequency); // clockspeed.c
+
 void setup() {
+#if defined(ARDUINO_TEENSY_DEVBRD5) || defined(ARDUINO_TEENSY_DEVBRD4) 
+  set_arm_clock(528000000 + 1);
+#endif
   Serial.begin(115200);
-  while (!Serial && millis() < 2500 );
-  if ( CrashReport) {
-    Serial.print( CrashReport);
-    while(1) delay(10);
+  while (!Serial && millis() < 2500)
+    ;
+  if (CrashReport) {
+    Serial.print(CrashReport);
+    while (1) delay(10);
   }
   Serial.println("\n" __FILE__ " " __DATE__ " " __TIME__);
   isEncrypt();
-  Serial.println( szTeensy );
-  Serial.println( );
+  Serial.println(szTeensy);
+  Serial.println();
   static char szPi[PI_DIGITS_SZ];
 
 #if defined(USB_DUAL_SERIAL)  // Send the Cascade code created to Second Serial for the indicated MakeCode( COUNT )
   buildAlpha();
-  MakeCode( 1000 );
+  MakeCode(1000);
   delay(500);
 #endif
 
   uint32_t piTime;
-  uint32_t theCount,iiCnt;
+  uint32_t theCount, iiCnt;
   piCycles = 0;
-  seePi( 200, NULL ); // Just for fun to see 800 Pi digits takes 9.1ms
+  seePi(200, NULL);  // Just for fun to see 800 Pi digits takes 9.1ms
   Serial.printf("\n\tNot enabled _isr() char[] Testing.\n");
   piTime = micros();
-  iiCnt = theCount = ThisFunc1( 0, seePi( PI_DIGITS, szPi ), &sumPi60dig );
+  iiCnt = theCount = ThisFunc1(0, seePi(PI_DIGITS, szPi), &sumPi60dig);
   piTime = micros() - piTime;
   Serial.printf("Cascading %lu calls took %lu us [%lu piCycles] : net %lu us\n", theCount, piTime, piCycles, piTime - piCycles / 600);
   piCycles = 0;
   piTime = micros();
-  int32_t seePiStart = seePi( PI_DIGITS, szPi );
-  while ( 0 < iiCnt ) {
-    ThisFunc0( 0, seePiStart, &sumPi60dig );
+  int32_t seePiStart = seePi(PI_DIGITS, szPi);
+  while (0 < iiCnt) {
+    ThisFunc0(0, seePiStart, &sumPi60dig);
     iiCnt--;
   }
   piTime = micros() - piTime;
   Serial.printf("Direct calls took %lu us [%lu piCycles] : net %lu us\n\n", piTime, piCycles, piTime - piCycles / 600);
   time_now = millis();
 
-  pinMode( LED_BUILTIN, OUTPUT ); // testAlpha Toggles on call to show running
-  uint32_t isrRate = 50; // microseconds between string test _isr
-  for ( int32_t ii = 0; ii < 3; ii++ ) {
+  pinMode(LED_BUILTIN, OUTPUT);  // testAlpha Toggles on call to show running
+  uint32_t isrRate = 50;         // microseconds between string test _isr
+  for (int32_t ii = 0; ii < 3; ii++) {
     Serial.printf("\tENABLED _isr() char[] Test @%lu us", isrRate);
     isrRate *= 2;
     isrCycles = 0;
     allCycles = ARM_DWT_CYCCNT;
-    Alpha.begin( testAlpha, isrRate );
-    while ( ARM_DWT_CYCCNT - allCycles < F_CPU_ACTUAL );
-    Alpha.end( );
+    Alpha.begin(testAlpha, isrRate);
+    while (ARM_DWT_CYCCNT - allCycles < F_CPU_ACTUAL)
+      ;
+    Alpha.end();
     allCycles = ARM_DWT_CYCCNT - allCycles;
-    Serial.printf("\t_isr Cycles %lu of %lu : CPU %%=%f\n", isrCycles, allCycles, (float)isrCycles / allCycles );
+    Serial.printf("\t_isr Cycles %lu of %lu : CPU %%=%f\n", isrCycles, allCycles, (float)isrCycles / allCycles);
   }
 
   piCycles = 0;
   isrCycles = 0;
   allCycles = ARM_DWT_CYCCNT;
-  Alpha.begin( testAlpha, isrRate );
+  Alpha.begin(testAlpha, isrRate);
   piTime = micros();
-  theCount = ThisFunc1( 0, seePi( PI_DIGITS, szPi ), &sumPi60dig );
+  theCount = ThisFunc1(0, seePi(PI_DIGITS, szPi), &sumPi60dig);
   piTime = micros() - piTime;
-  Alpha.end( );
+  Alpha.end();
   allCycles = ARM_DWT_CYCCNT - allCycles;
   Serial.printf("Cascading %lu calls took %lu us [%lu piCycles] : net {less Pi} %lu us\n", theCount, piTime, piCycles / 600, piTime - piCycles / 600);
-  Serial.printf("\t_isr Cycles %lu of %lu : CPU %%=%f\n", isrCycles, allCycles, (float)isrCycles / allCycles );
+  Serial.printf("\t_isr Cycles %lu of %lu : CPU %%=%f\n", isrCycles, allCycles, (float)isrCycles / allCycles);
   Serial.printf("Cascading %lu calls took %lu us : net {less isr} %lu us\n", theCount, piTime, piCycles, piTime - isrCycles / 600);
-  Serial.printf("Cascading %lu calls took %lu us : Cycles/call %lu\n", theCount, piTime, piTime/theCount);
+  Serial.printf("Cascading %lu calls took %lu us : Cycles/call %lu\n", theCount, piTime, piTime / theCount);
   piCycles = 0;
   piTime = micros();
   piCycles = 0;
   isrCycles = 0;
   allCycles = ARM_DWT_CYCCNT;
-  Alpha.begin( testAlpha, isrRate );
-  seePiStart = seePi( PI_DIGITS, szPi );
+  Alpha.begin(testAlpha, isrRate);
+  seePiStart = seePi(PI_DIGITS, szPi);
   iiCnt = theCount;
-  while ( 0 < iiCnt ) {
-    ThisFunc0( 0, seePiStart, &sumPi60dig );
+  while (0 < iiCnt) {
+    ThisFunc0(0, seePiStart, &sumPi60dig);
     iiCnt--;
   }
-  Alpha.end( );
+  Alpha.end();
   allCycles = ARM_DWT_CYCCNT - allCycles;
   piTime = micros() - piTime;
   Serial.printf("\nDirect calls took %lu us [%lu piCycles] : net {less Pi} %lu us\n", piTime, piCycles / 600, piTime - piCycles / 600);
-  Serial.printf("\t_isr Cycles %lu of %lu : CPU %%=%f\n", isrCycles, allCycles, (float)isrCycles / allCycles );
+  Serial.printf("\t_isr Cycles %lu of %lu : CPU %%=%f\n", isrCycles, allCycles, (float)isrCycles / allCycles);
   Serial.printf("Direct calls took %lu us [%lu piCycles] : net {less isr} %lu us\n", piTime, piCycles, piTime - isrCycles / 600);
-  Serial.printf("Direct calls took %lu us [%lu piCycles] : Cycles/call %lu\n", piTime, piCycles, piTime/theCount);
-  time_now = millis(); // for time measure in loop on TEMP
+  Serial.printf("Direct calls took %lu us [%lu piCycles] : Cycles/call %lu\n", piTime, piCycles, piTime / theCount);
+  time_now = millis();  // for time measure in loop on TEMP
 
 
   Serial.printf("\n\nEnd of setup() tests, send USB char for repeating loop() testing with temp display ...\n");
-  while (!Serial.available())yield();
+  while (!Serial.available()) yield();
   Serial.printf("\tENABLED _isr() char[] Test @%lu us", isrRate);
   piCycles = 0;
   isrCycles = 0;
   allCycles = ARM_DWT_CYCCNT;
-  Alpha.begin( testAlpha, isrRate );
+  Alpha.begin(testAlpha, isrRate);
 }
 
 void loop() {
   static char szPi[PI_DIGITS_SZ];
-  Serial.printf("\t%4.4f",(millis() - time_now) * 0.00001667); Serial.print(", ");
+  Serial.printf("\t%4.4f", (millis() - time_now) * 0.00001667);
+  Serial.print(", ");
   Serial.println(tempmonGetTemp(), 2);
-  for ( int32_t ii = 0; ii < 3; ii++) {
+  for (int32_t ii = 0; ii < 3; ii++) {
     uint32_t piTime;
     uint32_t theCount;
     piCycles = 0;
     piTime = micros();
-    theCount = ThisFunc1( 0, seePi( PI_DIGITS, szPi ), &sumPi60dig );
-    Serial.printf( "%s Completed CASCADE Count %lu\t", szTeensy, theCount );
+    theCount = ThisFunc1(0, seePi(PI_DIGITS, szPi), &sumPi60dig);
+    Serial.printf("%s Completed CASCADE Count %lu\t", szTeensy, theCount);
     piTime = micros() - piTime;
     Serial.printf("\nCascading took %lu us [%lu piCycles] : net %lu us\n", piTime, piCycles, piTime - piCycles / 600);
     piCycles = 0;
     piTime = micros();
-    int32_t seePiStart = seePi( PI_DIGITS, szPi );
-    while ( 0 < theCount ) {
-      ThisFunc0( 0, seePiStart, &sumPi60dig );
+    int32_t seePiStart = seePi(PI_DIGITS, szPi);
+    while (0 < theCount) {
+      ThisFunc0(0, seePiStart, &sumPi60dig);
       theCount--;
     }
     piTime = micros() - piTime;
     Serial.printf("Direct calls took %lu us [%lu piCycles] : net %lu us\n\n", piTime, piCycles, piTime - piCycles / 600);
-    if ( errAlpha( NULL, 0, 0 )) { // from ::   // testAlpha();
-      Serial.printf(" -----\t ALPHA FAIL %lu", errAlpha( NULL, 0, 0 ) ); // debug
+    if (errAlpha(NULL, 0, 0)) {                                        // from ::   // testAlpha();
+      Serial.printf(" -----\t ALPHA FAIL %lu", errAlpha(NULL, 0, 0));  // debug
       delay(500);
     }
   }
 }
 
-uint32_t errAlpha( const char *szBad, uint32_t ii, uint32_t kk  ) {
+uint32_t errAlpha(const char *szBad, uint32_t ii, uint32_t kk) {
   static uint32_t errCnt = 0;
   static int32_t iiE = 0, kkE = 0;
-  if ( NULL != szBad ) {
+  if (NULL != szBad) {
     //Serial.print(szBad); // debug
-    digitalToggleFast( LED_BUILTIN ); // double toggle will emphsize error pulse
+    digitalToggleFast(LED_BUILTIN);  // double toggle will emphsize error pulse
     errCnt++;
-    if ( iiE == 0 && kkE == 0 ) {
+    if (iiE == 0 && kkE == 0) {
       iiE = ii;
       kkE = kk;
     }
     // Serial.printf("\nAlpha Error %s @ ii==%d kk==%d\n\n", szBad, iiE, kkE ); // DEBUG - UNDER _ISR()
     delay(10);  // debug - makes LED toggle flicker
-  }
-  else if ( errCnt ) {
-    Serial.printf("\nAlpha Error @ ii==%d kk==%d\n\n", iiE, kkE );
+  } else if (errCnt) {
+    Serial.printf("\nAlpha Error @ ii==%d kk==%d\n\n", iiE, kkE);
     iiE = 0;
     kkE = 0;
     delay(100);  // debug
@@ -168,11 +175,11 @@ uint32_t errAlpha( const char *szBad, uint32_t ii, uint32_t kk  ) {
   return errCnt;
 }
 
-uint16_t piComp[] = {3141, 5926, 5358, 9793, 2384, 6264, 3383, 2795, 288, 4197, 1693, 9937, 5105, 8209, 7494, 4592, 3078, 1640, 6286, 2089, 9862, 8034, 8253, 4211, 7067, 9821, 4808, 6513, 2823, 664, 7093, 8446, 955, 582, 2317, 2535, 9408, 1284, 8111, 7450, 2841, 270, 1938, 5211, 555, 9644, 6229, 4895, 4930, 3819, 6442, 8810, 9756, 6593, 3446, 1284, 7564, 8233, 7867, 8316, 5271, 2019, 914, 5648, 5669, 2346, 348, 6104, 5432, 6648, 2133, 9360, 7260, 2491, 4127, 3724, 5870, 660, 6315, 5881, 7488, 1520, 9209, 6282, 9254, 917, 1536, 4367, 8925, 9036, 11, 3305, 3054, 8820, 4665, 2138, 4146, 9519, 4151, 1609, 4330, 5727, 365, 7595, 9195, 3092, 1861, 1738, 1932, 6117, 9310, 5118, 5480, 7446, 2379, 9627, 4956, 7351, 8857, 5272, 4891, 2279, 3818, 3011, 9491, 2983, 3673, 3624, 4065, 6643, 860, 2139, 4946, 3952, 2473, 7190, 7021, 7986, 943, 7027, 7053, 9217, 1762, 9317, 6752, 3846, 7481, 8467, 6694, 513, 2000, 5681, 2714, 5263, 5608, 2778, 5771, 3427, 5778, 9609, 1736, 3717, 8721, 4684, 4090, 1224, 9534, 3014, 6549, 5853, 7105, 792, 2796, 8925, 8923, 5420, 1995, 6112, 1290, 2196, 864, 344, 1815, 9813, 6297, 7477, 1309, 9605, 1870, 7211, 3499, 9999, 8372, 9780, 4995, 1059, 7317, 3281, 6096, 3185 };
+uint16_t piComp[] = { 3141, 5926, 5358, 9793, 2384, 6264, 3383, 2795, 288, 4197, 1693, 9937, 5105, 8209, 7494, 4592, 3078, 1640, 6286, 2089, 9862, 8034, 8253, 4211, 7067, 9821, 4808, 6513, 2823, 664, 7093, 8446, 955, 582, 2317, 2535, 9408, 1284, 8111, 7450, 2841, 270, 1938, 5211, 555, 9644, 6229, 4895, 4930, 3819, 6442, 8810, 9756, 6593, 3446, 1284, 7564, 8233, 7867, 8316, 5271, 2019, 914, 5648, 5669, 2346, 348, 6104, 5432, 6648, 2133, 9360, 7260, 2491, 4127, 3724, 5870, 660, 6315, 5881, 7488, 1520, 9209, 6282, 9254, 917, 1536, 4367, 8925, 9036, 11, 3305, 3054, 8820, 4665, 2138, 4146, 9519, 4151, 1609, 4330, 5727, 365, 7595, 9195, 3092, 1861, 1738, 1932, 6117, 9310, 5118, 5480, 7446, 2379, 9627, 4956, 7351, 8857, 5272, 4891, 2279, 3818, 3011, 9491, 2983, 3673, 3624, 4065, 6643, 860, 2139, 4946, 3952, 2473, 7190, 7021, 7986, 943, 7027, 7053, 9217, 1762, 9317, 6752, 3846, 7481, 8467, 6694, 513, 2000, 5681, 2714, 5263, 5608, 2778, 5771, 3427, 5778, 9609, 1736, 3717, 8721, 4684, 4090, 1224, 9534, 3014, 6549, 5853, 7105, 792, 2796, 8925, 8923, 5420, 1995, 6112, 1290, 2196, 864, 344, 1815, 9813, 6297, 7477, 1309, 9605, 1870, 7211, 3499, 9999, 8372, 9780, 4995, 1059, 7317, 3281, 6096, 3185 };
 // https://crypto.stanford.edu/pbc/notes/pi/code.html // Calc Pi to 800 digits
 
 int32_t r[2800 + 1];
-PI_INLINE uint32_t seePi( uint32_t maxDigits, char *szPi ) {
+PI_INLINE uint32_t seePi(uint32_t maxDigits, char *szPi) {
   int32_t i, k, b, d, pp = 0;
   int32_t c = 0;
   uint32_t piTime;
@@ -197,20 +204,19 @@ PI_INLINE uint32_t seePi( uint32_t maxDigits, char *szPi ) {
       if (i == 0) break;
       d *= i;
     }
-    if ( NULL != szPi ) {
-      szC += sprintf( &szPi[szC], "%.4ld", c + d / 10000);
-    }
-    else { // Show Pi 800 digits
+    if (NULL != szPi) {
+      szC += sprintf(&szPi[szC], "%.4ld", c + d / 10000);
+    } else {  // Show Pi 800 digits
       Serial.printf("%.4d", c + d / 10000);
-      if ( !(++pp % 16) ) Serial.print("\n\t");
+      if (!(++pp % 16)) Serial.print("\n\t");
     }
     vv = c + d / 10000;
     piSum += vv;
-    if ( vv != piComp[jj++] ) Serial.printf("\nmismatch at %lu : %lu != %lu \n", jj , vv, piComp[jj - 1]);
-    if ( maxDigits == jj ) break;
+    if (vv != piComp[jj++]) Serial.printf("\nmismatch at %lu : %lu != %lu \n", jj, vv, piComp[jj - 1]);
+    if (maxDigits == jj) break;
     c = d % 10000;
   }
-  if ( NULL == szPi ) {
+  if (NULL == szPi) {
     piTime = micros() - piTime;
     Serial.printf("\n\tdigit Sum = %lu in %lu us\n", piSum, piTime);
   }
@@ -237,13 +243,13 @@ PI_INLINE uint32_t seePi( uint32_t maxDigits, char *szPi ) {
 // 495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185
 
 extern char _stext[], _etext[], _sbss[], _ebss[], _sdata[], _edata[],
-       _estack[], _heap_start[], _heap_end[], _itcm_block_count[], *__brkval;
-extern "C" uint32_t  _sdataload; /* special linker symbols */
-extern const uint32_t hab_csf[]; // placeholder for HAB signature
+  _estack[], _heap_start[], _heap_end[], _itcm_block_count[], *__brkval;
+extern "C" uint32_t _sdataload;   /* special linker symbols */
+extern const uint32_t hab_csf[];  // placeholder for HAB signature
 PROGMEM char title_text[] = "Verify secure code is running properly";
 FLASHMEM int32_t isEncrypt() {
   int32_t ok = 0;
-  Serial.println( title_text );
+  Serial.println(title_text);
   Serial.println();
   char buf[21];
   uint32_t i, num;
@@ -304,14 +310,14 @@ FLASHMEM int32_t isEncrypt() {
     hab_size |= ((hab_csf[0] >> 8) & 0xFF) << 8;
     uint8_t hab_minor = (hab_csf[0] >> 24) & 7;
     Serial.printf("Pass: CSF is %u bytes with HAB version 4.%u\n", hab_size, hab_minor);
-    strcat( szTeensy, " ENC" );
+    strcat(szTeensy, " ENC");
   } else {
     Serial.println("Fail: csf not HAB 4.0");
-    strcat( szTeensy, " nor" );
+    strcat(szTeensy, " nor");
     ok--;
   }
-  const uint32_t hab_version = (*(uint32_t (**)())0x00200330)();
-  const uint32_t hab_status = (*(int32_t (**)(int32_t *, int32_t *))0x00200324)(NULL, NULL);
+  const uint32_t hab_version = (*(uint32_t(**)())0x00200330)();
+  const uint32_t hab_status = (*(int32_t(**)(int32_t *, int32_t *))0x00200324)(NULL, NULL);
   if (hab_version == 0x40307) {
     Serial.println("NOTE: hab_version == 0x40307");
   } else {
@@ -326,16 +332,16 @@ FLASHMEM int32_t isEncrypt() {
   }
   if ((HW_OCOTP_CFG5 & 0x04C00002) == 0x04C00002) {
     Serial.print("Secure mode IS set :: Fuses == 0x");
-    strcat( szTeensy, " SM:" );
+    strcat(szTeensy, " SM:");
   } else {
     Serial.print("Secure mode NOT SET :: Fuses == 0x");
-    strcat( szTeensy, " ns:" );
+    strcat(szTeensy, " ns:");
     ok--;
   }
   i = F_CPU_ACTUAL / 1000000;
-  sprintf( buf, " @%lu", i );
-  strcat( szTeensy, buf );
-  Serial.println( HW_OCOTP_CFG5, HEX );
+  sprintf(buf, " @%lu", i);
+  strcat(szTeensy, buf);
+  Serial.println(HW_OCOTP_CFG5, HEX);
 
   Serial.println();
   if (0 == ok) Serial.println("All Tests Passed.  :-)");
